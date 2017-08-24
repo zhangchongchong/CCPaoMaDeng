0156e1d1b5116fdb37cdfc64e3e7b74ffb85b52c
张冲 <zhangchongchong@zhangchongdeMacBook-Air.local>
2017-08-24 10:48:20 +0800

    shangchuan

--- 跑马灯/ViewController.m
+++ 跑马灯/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *PMDlabel;
 
 @end
 
@@ -16,6 +17,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+   __block CGRect frame =  _PMDlabel.frame;
+    frame.origin.x = self.view.frame.size.width;
+    [NSTimer scheduledTimerWithTimeInterval:0.05 repeats:YES block:^(NSTimer * _Nonnull timer) {
+        
+        frame.origin.x = frame.origin.x-1;
+        
+         if ((frame.origin.x+frame.size.width)<=0) {
+             frame.origin.x = self.view.frame.size.width;
+        }
+        _PMDlabel.frame =frame;
+        
+        
+    }];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
